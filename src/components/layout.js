5b3e7a922de16a5ef5d419975aa272556267c711
redskowa <redskowa@gmail.com>
2021-08-31 21:04:36 +0900

    Completed Part 2

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,51 +5,81 @@
  * See: https://www.gatsbyjs.com/docs/use-static-query/
  */
 
-import * as React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+// import * as React from "react"
+// import PropTypes from "prop-types"
+// import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
-import "./layout.css"
+// import Header from "./header"
+// import "./layout.css"
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
+// const Layout = ({ children }) => {
+//   const data = useStaticQuery(graphql`
+//     query SiteTitleQuery {
+//       site {
+//         siteMetadata {
+//           title
+//         }
+//       }
+//     }
+//   `)
 
+//   return (
+//     <>
+//       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+//       <div
+//         style={{
+//           margin: `0 auto`,
+//           maxWidth: 960,
+//           padding: `0 1.0875rem 1.45rem`,
+//         }}
+//       >
+//         <main>{children}</main>
+//         <footer
+//           style={{
+//             marginTop: `2rem`,
+//           }}
+//         >
+//           © {new Date().getFullYear()}, Built with
+//           {` `}
+//           <a href="https://www.gatsbyjs.com">Gatsby</a>
+//         </footer>
+//       </div>
+//     </>
+//   )
+// }
+
+// Layout.propTypes = {
+//   children: PropTypes.node.isRequired,
+// }
+
+// export default Layout
+
+import * as React from 'react'
+import { Link } from 'gatsby'
+import { 
+  container,
+  heading,
+  navLinks,
+  navLinkItem,
+  navLinkText
+ } from './layout.module.css'
+
+const Layout = ({ pageTitle, children}) => {
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `2rem`,
-          }}
-        >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
-    </>
+    <div className={container}>
+      <title>{pageTitle}</title>
+      <nav>
+        <ul className={navLinks}>
+          <li className={navLinkItem}><Link to="/" className={navLinkText}>Home</Link></li>
+          <li className={navLinkItem}><Link to="/about" className={navLinkText}>About</Link></li>
+        </ul>
+      </nav>
+      <main>
+        <h1 className={heading}>{pageTitle}</h1>
+        {children}
+      </main>
+    </div>
   )
-}
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
 }
 
 export default Layout
---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+    return (
+        <Layout pageTitle="About Me">
+            <p>Hello and thank you for visiting.  This site is a work in progress...</p>
+        </Layout>
+    )
+}
+
+export default AboutPage
